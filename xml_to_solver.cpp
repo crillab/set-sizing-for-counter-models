584dd1061dce0eb400bd079c4cde6f6e1bfe2fed
Daniel Crowley <daniel@debian-crowley>
2024-05-28 13:18:03 +0200

    Finds relevant predicates.

--- xml_to_solver.cpp
+++ xml_to_solver.cpp
@@ -2,6 +2,7 @@
 
 #include <algorithm>
 #include <array>
+#include <chrono>
 #include <iostream>
 #include <map>
 #include <sstream>
@@ -11,6 +12,8 @@
 using xml_node = const pugi::xml_node &;
 
 static int k; // upper-bound for size of abstract sets
+
+enum typref { POW_INTEGER, INTEGER };
 
 class Formula {
   std::map<std::string, std::array<int, 2>> sets;    // <"name", {start_var, max_size}>
@@ -37,7 +40,34 @@ public:
   void print_cnf () {
     std::cout << cnf_body.str ();
   }
+
+  int predicates {0};
 };
+
+inline bool convexity_constraint (xml_node comparison) {
+  if (std::string {"Exp_Comparison"} != comparison.name ()
+      || std::string {"="} != comparison.attribute ("op").value ()
+      || std::string {"Id"} != comparison.first_child ().name ())
+    { return false; }
+  
+  std::string value {comparison.first_child ().attribute ("value").value ()};
+  xml_node second_child {comparison.first_child ().next_sibling ()};
+  
+  return
+    std::string {".."} == second_child.attribute ("op").value () &&
+    std::string {"imin"} == second_child.first_child ().attribute ("op").value () &&
+    std::string {"imax"} == second_child.first_child ().next_sibling ().attribute ("op").value () &&
+    std::ranges::all_of (second_child.children (),
+			  [&value] (auto &child) { return value == child.first_child ().attribute ("value").value (); });
+}  
+  
+std::string get_cnf_file_name (char *pog_name) {
+  std::string read_in_file {pog_name};
+  read_in_file = read_in_file.substr (read_in_file.rfind ('/', read_in_file.rfind ('/') - 1) + 1);
+  std::ranges::replace (read_in_file, '/', '-');
+  read_in_file.replace (read_in_file.rfind ("pog"), 3, "cnf");
+  return read_in_file;
+}
 
 void Formula::apply_order_encoding (bool non_empty) {
   for (auto &set : sets) {
@@ -56,7 +86,7 @@ void Formula::explore_context (xml_node proof_obligations) {
 			 [this] (auto definition) {
 			   std::string name {definition.attribute ("name").value ()};
 			   if (std::string {"Define"} == definition.name ()
-			       && (name == "ctx"                                        // CHECK
+			       && (name == "ctx"                                        // CHECK // Different from POGParser
 				   // || name == "ass"                                  // CHECK
 				   || name == "sets"                                    // CHECK
 				   || name.substr (name.length () - 3) == "prp")) {
@@ -64,12 +94,41 @@ void Formula::explore_context (xml_node proof_obligations) {
 			       if (std::string {"Set"} == interior.name ())
 				 { handle_set (interior); }
 			       else
-				 { /*handle_predicate (interior);*/ }
+				 { handle_predicate (interior); }
 			     }
 			   }});
 }
 
+void Formula::handle_predicate (xml_node predicate) {
+  bool skip
+    {
+      // Deal with only POW (Z) and Z
+      std::ranges::any_of (predicate.children (),
+			   [] (auto child) { return child.attribute ("typref").as_int () > INTEGER; }) ||
+      // Need some sets
+      std::ranges::all_of (predicate.children (),
+			   [] (auto child) { return child.attribute ("typref").as_int () == INTEGER; }) ||
+      // Skip x = min (x)..max (x)
+      convexity_constraint (predicate)
+    };
+  
+  if (skip)
+    { return; }
+
+  if (std::string {"="} == predicate.attribute ("op").value () && std::string {"s749"} == predicate.first_child ().attribute ("value").value ())
+    { predicate.print (std::cout);
+      std::cout << convexity_constraint (predicate) << '\n';
+      abort ();
+    }
+  ++predicates;
+  predicate.print (std::cout);
+}
+
 void Formula::handle_set (xml_node set) {
+  // Only POW (Z)
+  if (set.child ("Id").attribute ("typref").as_int () != 0)
+    { return; }
+  
   std::string id {set.child ("Id").attribute ("value").value ()};
 
   int size {upper_bound};
@@ -96,14 +155,6 @@ void Formula::order_encode_range (int zero, int size) {
     { make_clause ({-(zero + i), zero + i + 1}); }
 }
 
-std::string get_cnf_file_name (char *pog_name) {
-  std::string read_in_file {pog_name};
-  read_in_file = read_in_file.substr (read_in_file.rfind ('/', read_in_file.rfind ('/') - 1) + 1);
-  std::ranges::replace (read_in_file, '/', '-');
-  read_in_file.replace (read_in_file.rfind ("pog"), 3, "cnf");
-  return read_in_file;
-}
-
 int main (int argc, char **argv) {
   k = atoi (argv[1]);
   std::string out_file {get_cnf_file_name (argv[2])};
@@ -115,7 +166,8 @@ int main (int argc, char **argv) {
 
   formula.explore_context (doc.first_child ());
   formula.apply_order_encoding ();
-  // formula.print_cnf ();
+  std::cout << formula.get_next_var () << '\n';
+  std::cout << formula.predicates << '\n';
   
   return 0;
 }
